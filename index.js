2a64e2a66e8144ec10a0d67af95231ba362d0bf2
Nandish <nandish.bs@gmail.com>
2024-04-24 20:15:04 -0400

    feat: pdf update

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const hummus = require('hummus-recipe')
+const hummus = require('hummus-recipe');
+const fs = require('fs');
 
-const pdfArray = ['C:\\pdfFiles\\nov2019.pdf','C:\\pdfFiles\\dec2019.pdf','C:\\pdfFiles\\jan2020.pdf']
+const pdfstobemerged = fs.readdirSync(`C:\\pdffiles\\tobemerged`)
 const outputPDF = 'C:\\pdfFiles\\merged.pdf';
+const outputPDFWithPageNum = 'C:\\pdfFiles\\mergedPageNum.pdf';
 
-const pdf = new hummus(pdfArray[0], outputPDF);
+const pdf = new hummus(`C:\\pdffiles\\tobemerged\\${pdfstobemerged[0]}`, outputPDF);
+let i = 1;
 
-let i=1;
+for (i = 1; i < pdfstobemerged.length; i++) {
+    if (pdfstobemerged[i].includes('.pdf')) {
+        pdf
+            .appendPage(`C:\\pdffiles\\tobemerged\\${pdfstobemerged[i]}`)
+    }
 
-for(i=1;i<pdfArray.length;i++)
-{
-    pdf.appendPage(pdfArray[i])
 }
 pdf.endPDF();
+
+const pdfNew = new hummus(outputPDF, outputPDFWithPageNum);
+const info = pdfNew.info()
+for (i = 1; i < pdfNew.metadata.pages; i++) {
+    pdfNew
+        .editPage(i)
+        .text(`Page No: ${i.toString()}`, 450, 750, {color:"#000000"})
+        .endPage()
+}
+pdfNew.endPDF();
+
